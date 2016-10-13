9da6f0edb41202f8cc76573f790fed2641a480c8
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2016-10-13 18:08:50 +0300

    'test'

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
+
 import App from './components/App';
 import HomePage from './components/HomePage';
 import FuelSavingsPage from './containers/FuelSavingsPage'; // eslint-disable-line import/no-named-as-default
